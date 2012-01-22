eb692a21ff04ef6540efd90c12ebf4d286fce442
Ken Inglis <inglis.ken@googlemail.com>
2012-01-22 15:43:42 +0100

    key events and player

--- core/game.js
+++ core/game.js
@@ -5,7 +5,33 @@ kai.game = function(startLevel) {
     var level = startLevel;
     var start = function() {
         console.debug('start');
+        loop();
     };
+
+    // http://nokarma.org/2011/02/27/javascript-game-development-keyboard-input/index.html
+    var key = {
+        pressed:{},
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40,
+        isDown: function(code) { return this.pressed[code]; },
+        onKeydown: function(e) { this.pressed[e.keyCode] = true; },
+        onKeyup: function(e) { delete this.pressed[e.keyCode]; }
+    };
+    window.addEventListener('keyup', function(e) { key.onKeyup(e); }, false);
+    window.addEventListener('keydown', function(e) { key.onKeydown(e); }, false);
+
+
+
+    var turn = 0;
+    var loop = function(){
+        turn++;
+        level.update(key,turn);
+        level.draw();
+        setTimeout(loop, 50);
+    };
+
     return {
         start:start
     }
--- levels/awaken.js
+++ levels/awaken.js
@@ -6,8 +6,37 @@ kai.levels = kai.levels || {};
 kai.levels.awaken = function(board) {
     console.debug('awaken');
 
-    var update = function() {};
-    var draw = function() {};
+    var player = {
+        x: 10, y: 10,
+        v: 2,
+        arcEnd: 2 * Math.PI,
+        draw: function(ctx) {
+            ctx.beginPath(); 
+            ctx.arc(this.x,this.y,5,0,this.arcEnd);
+            ctx.fillStyle = 'green';
+            ctx.fill();
+        },
+        up: function(){this.y -= this.v;},
+        down: function(){this.y += this.v;},
+        left: function(){this.x -= this.v;},
+        right: function(){this.x += this.v;},
+    };
+
+    var update = function(key,turn) {
+        if(key.isDown(key.UP)) { player.up(); }
+        if(key.isDown(key.DOWN)) { player.down(); }
+        if(key.isDown(key.LEFT)) { player.left(); }
+        if(key.isDown(key.RIGHT)) { player.right(); }
+    };
+
+    var clearCtx = function(ctx) {
+        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+    };
+
+    var draw = function() {
+        clearCtx(board.actors);
+        player.draw(board.actors);
+    };
 
     return {
         update:update,
