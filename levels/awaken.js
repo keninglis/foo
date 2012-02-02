ebaabde0532fecf16d8ec37ed90efa433b25f114
Ken Inglis <inglis.ken@googlemail.com>
2012-02-02 23:55:40 +0100

    use sample background

--- levels/awaken.js
+++ levels/awaken.js
@@ -7,64 +7,30 @@ kai.levels.awaken = function(board) {
     console.debug('awaken');
 
     var boardWidth = 500; var boardHeight = 500;
-    var tileWidth = 25;
+    var tileWidth = boardWidth / 20;
     var halfWindowHeight = Math.floor(board.actors.canvas.height / 2);
     var halfWindowWidth = Math.floor(board.actors.canvas.width / 2);
     var windowX = 0; var windowY = 0;
 
-    var rooms = [
-        // the border
-        { 
-            x:0,y:0,x2:boardWidth,y2:boardHeight,
-            draw: function(ctx) { 
-                ctx.strokeRect(this.x,this.y,this.x2,this.y2);
-            }
-        },
-        { 
-            x:0,y:0,x2:6*tileWidth,y2:4*tileWidth,
-            title: 'awaken', 
-            draw: function(ctx) { 
-                ctx.strokeRect(this.x,this.y,this.x2,this.y2);
-            }
-        },
-        { 
-            x:0,y:4*tileWidth,x2:4*tileWidth,y2:6*tileWidth,
-            title: 'awaken', 
-            draw: function(ctx) { 
-                ctx.strokeRect(this.x,this.y,this.x2,this.y2);
-            }
-        },
-        { 
-            x:0,y:10*tileWidth,x2:4*tileWidth,y2:6*tileWidth,
-            title: 'awaken', 
-            draw: function(ctx) { 
-                ctx.strokeRect(this.x,this.y,this.x2,this.y2);
-            }
-        },
-        { 
-            x:0,y:16*tileWidth,x2:6*tileWidth,y2:4*tileWidth,
-            title: 'awaken', 
-            draw: function(ctx) { 
-                //ctx.fillStyle = 'rgba(255,255,255,.3)';
-                //ctx.fillRect(this.x,this.y,this.x2,this.y2);
-                ctx.strokeRect(this.x,this.y,this.x2,this.y2);
-            }
-        }
+    var floorPlan = false;
 
-    ];
-
+    // rewrite function: load image and then draw  
     var drawBackground = function(ctx) {
-        ctx.save();
-        ctx.translate(-windowX,-windowY);
-        for(i in rooms) {
-            rooms[i].draw(ctx);
-        }
-        ctx.restore();
+        drawBackground = function() { 
+            floorPlan = new Image(); 
+            floorPlan.src = 'levels/awaken_trans.png';
+            floorPlan.onload = function() {
+                drawBackground = function(ctx) {
+                    ctx.drawImage(floorPlan,-windowX,-windowY);
+                };
+            };
+            drawBackground = function(){};
+        };
     };
 
     var player = {
-        x: 100, y: 300,
-        v: 8,
+        x: 290, y: 885,
+        v: 10,
         w: 6,
         arcEnd: 2 * Math.PI,
         draw: function(ctx) {
@@ -90,24 +56,16 @@ kai.levels.awaken = function(board) {
     var update = function(key,turn) {
         var moved = false;
         if(key.isDown(key.UP)) { 
-            if(player.y > 0) {
-                player.up(); moved = true; 
-            }
+            player.up(); moved = true; 
         }
         if(key.isDown(key.DOWN)) { 
-            if(player.y < boardHeight) {
-                player.down(); moved = true; 
-            }
+            player.down(); moved = true; 
         }
         if(key.isDown(key.LEFT)) { 
-            if(player.x > 0) {
-                player.left(); moved = true; 
-            }
+            player.left(); moved = true; 
         }
         if(key.isDown(key.RIGHT)) { 
-            if(player.x < boardWidth) {
-                player.right(); moved = true; 
-            }
+            player.right(); moved = true; 
         }
         if(moved) {
             moveWindow();
