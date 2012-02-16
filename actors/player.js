ef76ddc96c1d0b0585dbe1caf6aa8cd523923096
Ken Inglis <inglis.ken@googlemail.com>
2012-02-16 19:35:42 +0100

    use the constructor luke

--- actors/player.js
+++ actors/player.js
@@ -1,31 +1,48 @@
 var kai = kai || {};
 
-kai.player = function(input) {
-
-    // what do you want to do this turn
-    var requestAction = function(){
-        if(input.isDown(input.UP)) {
-            console.debug('up');
-        }
-        if(input.isDown(input.DOWN)) {
-            console.debug('down');
-        }
-        if(input.isDown(input.LEFT)) {
-            console.debug('left');
-        }
-        if(input.isDown(input.RIGHT)) {
-            console.debug('right');
-        }
-        //console.debug('player requestAction');
-    };
-
-    // what happened to you this turn
-    var act = function(response) {
-        //console.debug('player act');
-    };
-
-    return {
-        requestAction: requestAction,
-        act: act
-    };
+// constructor
+kai.Player = function(input) {
+    
+    this.input = input;
+    this.x = 20;
+    this.y = 20;
+    this.bearing = 0;
+    this.r = 6; //radius
+    this.arcEnd = 2 * Math.PI; // full circle
 }
+
+kai.Player.prototype.setWindow = function(midX,midY,ctx) {
+    // adjust midX by radius?
+    this.winX = midX;
+    this.winY = midX;
+    this.ctx = ctx;
+};
+
+// what do you want to do this turn
+kai.Player.prototype.requestAction = function(){
+    if(this.input.isDown(this.input.UP)) {
+        console.debug('up');
+    }
+    if(this.input.isDown(this.input.DOWN)) {
+        console.debug('down');
+    }
+    if(this.input.isDown(this.input.LEFT)) {
+        console.debug('left');
+    }
+    if(this.input.isDown(this.input.RIGHT)) {
+        console.debug('right');
+    }
+    //console.debug('player requestAction');
+};
+
+// what happened to you this turn
+kai.Player.prototype.act = function(response) {
+    //console.debug('player act');
+};
+
+kai.Player.prototype.draw = function() {
+    this.ctx.beginPath(); 
+    this.ctx.arc(this.winX,this.winY,this.r,0,this.arcEnd);
+    this.ctx.fillStyle = 'green';
+    this.ctx.fill();
+};
