2f37a9bfb6b4ab7af90769051411965f777c6d79
Ken Inglis <inglis.ken@googlemail.com>
2012-02-16 19:35:21 +0100

    add xy to zombies

--- actors/zombies.js
+++ actors/zombies.js
@@ -3,6 +3,9 @@ kai.zombies = kai.zombies || {};
 
 kai.zombies.normal = function(x,y,bearing) {
 
+    this.x = x;
+    this.y = y;
+    this.bearing = bearing; 
     // what do you want to do this turn
     var requestAction = function(){
         //console.debug('zombie requestAction');
@@ -12,10 +15,13 @@ kai.zombies.normal = function(x,y,bearing) {
     var act = function(response) {
         //console.debug('zombie act');
     };
-
+    
     return {
         requestAction: requestAction,
-        act: act
+        act: act,
+        x:this.x,
+        y:this.y,
+        bearing:this.bearing
     };
 
 }
