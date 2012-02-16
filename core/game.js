d20716c9c349c292af3803c1a0899105f5bc1eee
Ken Inglis <inglis.ken@googlemail.com>
2012-02-16 19:34:01 +0100

    separate player from actors

--- core/game.js
+++ core/game.js
@@ -3,13 +3,17 @@ var kai = kai || {};
 kai.game = function(board,player,startLevel) {
     var actors;
     var level;
+
+    // middle points of visible screen
+    var windowHalfW = board.actors.canvas.width / 2;
+    var windowHalfH = board.actors.canvas.height / 2;
     
     var changeLevel = function(newLevel) {
         level = newLevel;
         // @TODO add actors from level
         actors = level.getActors();
+        player.setWindow(windowHalfW, windowHalfH, board.actors);
         // @TODO place player on new level        
-        actors.push(player);
     };
     changeLevel(startLevel); // initial level
 
@@ -19,15 +23,18 @@ kai.game = function(board,player,startLevel) {
 
     var turn = 0;
     var loop = function(){
-        if(!(turn % 50)) { console.debug('turn',turn); }
-            
         turn++;
-        // ask actors what they're doing, carry out the response from the level
+        if(!(turn % 50)) { console.debug('turn',turn); }
+
+        // what ya doing? this happened. How d'ya like that?
+        player.act(level.tryAction(player.requestAction()));
         for(var i in actors) {
-            var response = level.tryAction(actors[i].requestAction());
-            // opportunity to delay response till end of loop
-            actors[i].act(response);    
+            actors[i].act(level.tryAction(actors[i].requestAction()));
         }
+       
+        player.draw(); 
+
+
         setTimeout(loop, 200);
     };
     
